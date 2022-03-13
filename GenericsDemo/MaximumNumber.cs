b7164b311700e4aceaf082c0799b72c668cbbeb5
sonali4598 <sonaliagivale11093@gmail.com>
2022-03-13 22:55:24 +0530

    Refactor max method

--- GenericsDemo/MaximumNumber.cs
+++ GenericsDemo/MaximumNumber.cs
@@ -8,54 +8,11 @@ namespace GenericsDemo
 {
     internal class MaximumNumber<T> where T : IComparable
     {
-        private T[] array;
-        public MaximumNumber(T[] array)
+        public static void Max(T var1, T var2, T var3)
         {
-            this.array = array;
-        }
-        public void Max(params T[] array)
-        {
-            this.array = array;
+            T[] array = new T[] { var1, var2, var3 };
             Array.Sort(array);
-            PrintMax(array);
+            Console.WriteLine($"Max value is: {array[2]}");
         }
-
-        /// <summary>
-        /// Prints the maximum.
-        /// </summary>
-        private void PrintMax(T[] value)
-        {
-            Console.WriteLine($"Max value is: {value[^1]}");
-        }
-        //public static void FindIntMax(int num1, int num2, int num3)
-        //{
-        //    if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
-        //        Console.WriteLine($"Max value is: {num1}");
-        //    else if (num2.CompareTo(num3) >= 0)
-        //        Console.WriteLine($"Max value is: {num2}");
-        //    else
-        //        Console.WriteLine($"Max value is: {num3}");
-        //}
-
-        ////Find maximum using Float
-        //public static void FloatMax(float num1, float num2, float num3)
-        //{
-        //    if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
-        //        Console.WriteLine($"Max value is: {num1}");
-        //    else if (num2.CompareTo(num3) >= 0)
-        //        Console.WriteLine($"Max value is: {num2}");
-        //    else
-        //        Console.WriteLine($"Max value is: {num3}");
-        //}
-        ////Find Maximum Using String
-        //public static void StringMax(string str1, string str2, string str3)
-        //{
-        //    if (str1.CompareTo(str2) >= 0 && str1.CompareTo(str3) >= 0)
-        //        Console.WriteLine($"Max value is: {str1}");
-        //    else if (str2.CompareTo(str3) >= 0)
-        //        Console.WriteLine($"Max value is: {str2}");
-        //    else
-        //        Console.WriteLine($"Max value is: {str3}");
-        //}
     }
 }
