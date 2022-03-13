e4296df72f469b8cca7cc810ca2ada02b7dd24f7
sonali4598 <sonaliagivale11093@gmail.com>
2022-03-13 22:39:05 +0530

    Extended Maximum method to take multiple parameter

--- GenericsDemo/MaximumNumber.cs
+++ GenericsDemo/MaximumNumber.cs
@@ -6,37 +6,48 @@ using System.Threading.Tasks;
 
 namespace GenericsDemo
 {
-    internal class MaximumNumber
+    internal class MaximumNumber<T> where T : IComparable
     {
-        public static void FindIntMax(int num1, int num2, int num3)
+        private T[] array;
+        public MaximumNumber(T[] array)
         {
-            if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
-                Console.WriteLine($"Max value is: {num1}");
-            else if (num2.CompareTo(num3) >= 0)
-                Console.WriteLine($"Max value is: {num2}");
-            else
-                Console.WriteLine($"Max value is: {num3}");
+            this.array = array;
         }
+        public void Max(params T[] array)
+        {
+            this.array = array;
+            Array.Sort(array);
+            Console.WriteLine($"Max value is: {array[2]}");
+        }
+        //public static void FindIntMax(int num1, int num2, int num3)
+        //{
+        //    if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
+        //        Console.WriteLine($"Max value is: {num1}");
+        //    else if (num2.CompareTo(num3) >= 0)
+        //        Console.WriteLine($"Max value is: {num2}");
+        //    else
+        //        Console.WriteLine($"Max value is: {num3}");
+        //}
 
-        //Find maximum using Float
-        public static void FloatMax(float num1, float num2, float num3)
-        {
-            if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
-                Console.WriteLine($"Max value is: {num1}");
-            else if (num2.CompareTo(num3) >= 0)
-                Console.WriteLine($"Max value is: {num2}");
-            else
-                Console.WriteLine($"Max value is: {num3}");
-        }
-        //Find Maximum Using String
-        public static void StringMax(string str1, string str2, string str3)
-        {
-            if (str1.CompareTo(str2) >= 0 && str1.CompareTo(str3) >= 0)
-                Console.WriteLine($"Max value is: {str1}");
-            else if (str2.CompareTo(str3) >= 0)
-                Console.WriteLine($"Max value is: {str2}");
-            else
-                Console.WriteLine($"Max value is: {str3}");
-        }
+        ////Find maximum using Float
+        //public static void FloatMax(float num1, float num2, float num3)
+        //{
+        //    if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
+        //        Console.WriteLine($"Max value is: {num1}");
+        //    else if (num2.CompareTo(num3) >= 0)
+        //        Console.WriteLine($"Max value is: {num2}");
+        //    else
+        //        Console.WriteLine($"Max value is: {num3}");
+        //}
+        ////Find Maximum Using String
+        //public static void StringMax(string str1, string str2, string str3)
+        //{
+        //    if (str1.CompareTo(str2) >= 0 && str1.CompareTo(str3) >= 0)
+        //        Console.WriteLine($"Max value is: {str1}");
+        //    else if (str2.CompareTo(str3) >= 0)
+        //        Console.WriteLine($"Max value is: {str2}");
+        //    else
+        //        Console.WriteLine($"Max value is: {str3}");
+        //}
     }
 }
--- GenericsDemo/Program.cs
+++ GenericsDemo/Program.cs
@@ -12,14 +12,11 @@ namespace GenericsDemo
         {
             Console.WriteLine("----------Find Max using Generics----------");
 
-            // Testing max value in 3 different positions
-            Console.WriteLine("Test case 1: " + TestCase.GetString(TestCase.IntTestCase1));
-            MaximumNumber.FindIntMax(TestCase.IntTestCase1[0], TestCase.IntTestCase1[1], TestCase.IntTestCase1[2]);
-            Console.WriteLine("Test case 2: " + TestCase.GetString(TestCase.IntTestCase2));
-            MaximumNumber.FindIntMax(TestCase.IntTestCase2[0], TestCase.IntTestCase2[1], TestCase.IntTestCase2[2]);
-            Console.WriteLine("Test case 3: " + TestCase.GetString(TestCase.IntTestCase3));
-            MaximumNumber.FindIntMax(TestCase.IntTestCase3[0], TestCase.IntTestCase3[1], TestCase.IntTestCase3[2]);
+            //Testing max value in 3 different positions
+            
             Console.WriteLine("--------------------------------------");
+            Console.WriteLine("*******Int Tests********");
+            TestCase.IntTest();
             Console.WriteLine("*******Float tests*******");
             TestCase.FloatTest();
             Console.WriteLine("*******String Test*******");
